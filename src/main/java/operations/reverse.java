880502cbc19ec759bb57da294f2923cda27e02d2
Devaki01 <devakijoshi.2005@gmail.com>
2026-04-06 11:55:49 +0530

    operations

---
+++ src/main/java/operations/reverse.java
@@ -0,0 +1,12 @@
+public class reverse {
+    public static void main(String[] args) {
+        int n = 512;
+
+        while(n != 1){
+            System.out.print(n + " ");
+            n = n/2;
+        }
+
+    }
+}
+
